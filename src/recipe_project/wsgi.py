44ccfcce1133864cf33543a4e8aea4db6b334ea9
haleytolar <Haleytolarwrites@outlook.com>
2024-12-16 09:50:42 -0600

    final touches

--- src/recipe_project/wsgi.py
+++ src/recipe_project/wsgi.py
@@ -1,13 +1,7 @@
 import os
-import sys
-
-# Add the 'src' directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
 
-# Create the WSGI application
 application = get_wsgi_application()
